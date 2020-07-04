a528dd0f2917b6ed355a4f0644f3154d57a6b970
cool247 <sadanandgpl@gmail.com>
2020-07-04 14:39:15 +0530

    Updates

---
+++ precache-manifest.8c50cac32577b01713985472ca908569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9c3ba9ce79c34453ee2db7e11499c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ff4ce754811a2b6cc3",
+    "url": "/static/css/main.499e78d4.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23ff4ce754811a2b6cc3",
+    "url": "/static/js/main.f708b1cb.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
